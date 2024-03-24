c688952992bc61e88255a9a857b4196e50e5e847
Simphiwe06 <simphiwembuli06@gmail.com>
2024-03-24 19:04:35 +0200

    linked data and styles

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,26 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <BarGraph :data="chartData" />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import BarGraph from './components/BarGraph.vue';
+import barGraphData from '../public/barGraph.json';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    BarGraph
+  },
+  data() {
+    return {
+      chartData: barGraphData
+    };
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+/* Add global styles here if needed */
 </style>
